6991fea3d1e51efee4a98909c6de9fd6116e6de1
Simone <simoneserpi@hotmail.it>
2022-03-22 17:16:55 +0100

    Somma

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 const selezione = prompt ("Scegli Pari o dispari") ;
 const numero = prompt("Scegli un numero compreso tra 1 e 9")
-let numeroPC = Math.floor(Math.random() * 10);
+const numeroPC = Math.floor(Math.random() * 10);
 let somma = 0;
 let pippo = 0;
 
 console.info(numeroPC);
 
 
-somma = numero+numeroPC;
+somma = numero + numeroPC;
 alert("la somma è" + somma);
 pippo= somma%2;
 
 switch (selezione)
 {
-    case "pari":
+    case "pari || Pari":
     if ( pippo == 0)
-     
     {
         alert("Hai vinto");
     }
     else { alert("Hai perso") }
 
-    case "dispari" :
+    case "dispari || Dispari" :
     if (pippo==1)
     {
         alert("Hai vinto");
